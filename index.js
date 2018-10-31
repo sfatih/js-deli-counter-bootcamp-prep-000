951541e488f836a187cbe19501252a99bd42efb8
semih Fatih <s_fatihh@hotmail.com>
2018-10-31 17:47:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function takeANumber(line, newPerson){
+  line.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${line.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if( katzDeliLine.length !== 0 ){
+    let temp = katzDeliLine[0];
+    katzDeliLine.splice(0,1);
+    return `Currently serving ${temp}.`;
+  }else{
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(line){
+  if(line.lenght !== 0 ){
+    let string = "The line is currently: "
+    for(let i=0;i<line.length;i++){
+      string +=  `${i+1}. ${line[i]}, `;
+    }
+    return string;
+  }else{
+    return "The line is currently empty.";
+  }
+}
